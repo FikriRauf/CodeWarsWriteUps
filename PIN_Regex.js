7dff6ad4ef4508001842b989c8dd86ebb6b45663
FikriRauf <azahararif@gmail.com>
2022-07-22 17:53:13 +0800

    removed unused comments

--- PIN_Regex.js
+++ PIN_Regex.js
@@ -9,77 +9,13 @@
 // "a234"   -->  false
 
 
-let pinp = 1234;
-let pinpp = 1342;
-const re = /^\d{4,6}$/g;
-const reg = /\w^a-zA-Z\d{4}/;
-const rege = /\d{4,6}/;
-const regex = /\W/; // /^a-zA-Z0-9_/
-const r = /\W\d{4,6}/;
+let pin = 1234;
+const regex = /\d{4,6}/;
 
-// if (rege.test(pin)) {
-//   console.log(true);
-// } else {
-//   console.log(false);
-// }
-
-// console.log(r.test(pin));
-
-pinLength = pinp.toString().length;
+pinLength = pin.toString().length;
 
 if (pinLength == 6 || pinLength == 4) {
-  console.log(pinLength == 6 || pinLength == 4)
-  console.log(pinLength)
-  console.log(r.test(pinp) + " hello")
+  console.log(r.test(pin))
 } else {
   console.log(false);
 }
-
-
-
-// if (pin.toString().length == 4 || pin.toString().length == 6) {
-//     if (pin > 6) {
-//         console.log(true);
-//     } else {
-//         console.log(false);
-//     }
-// }
-
-
-
-// // function holdFunc () {
-// //     if (typeof(pin) == 'number') {
-// //         if (pin !== -pin) {
-// //             if (pin.length == 4 || pin.length == 6) {
-// //                 if (pin < 0) {
-// //                     if (pin.length < 4 || pin.length > 6) {
-// //                         return true;
-// //                     }
-// //                 }
-// //             }
-// //         }
-// //     } else {
-// //         return false;
-// //     }  
-// // 
-// // 
-// if (typeof(pin) == 'number') {
-//   return true;
-// } else if (pin !== -pin) {
-//   return false;
-// } else if (pin.length == 4 || pin.length == 6) {
-//   return true;
-// } else if (pin < 0) {
-//   return false;
-// } else if (pin.length < 4 || pin.length > 6) {
-//   return false;
-// } else {
-//   return false;
-// }
-
-
-// // takes care of : -1.234 & a234
-// if (pin !== -pin) {
-//     return true;
-// }
-
