f2b3bb0226e02bb2ee15a1bb2e367e90533d4ef9
FikriRauf <azahararif@gmail.com>
2022-09-12 02:15:31 +0800

    attempt 01

--- Stop_gninnipS_My_sdroW.js
+++ Stop_gninnipS_My_sdroW.js
@@ -1,3 +1,4 @@
+// Introduction:
 // Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 // 
 // Examples:
@@ -7,3 +8,22 @@
 // spinWords( "This is another test" )=> returns "This is rehtona test"
 
 
+// Notes:
+// split sentence into each words
+// store each words into a variable
+// algorithm
+//     check each words if >= 5 characters
+//     reverse it if condition true
+// combine = unreverse_word + reverse_word
+
+
+// if (word >= 5) {
+//     reverse it
+// }
+
+
+const rege = /\w{5,}/;
+
+let wordSpeech = "Hey fellow warriors";
+
+console.log(wordSpeech.matchAll(rege));
